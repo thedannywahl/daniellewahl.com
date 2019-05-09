93fadcef0e82850dcfc83c8ea5467205307be7b6
Danny Wahl <danny@iywahl.com>
2019-05-09 12:16:29 -0600

    v2

---
+++ js/app.js
@@ -0,0 +1,33 @@
+const toggleActive = (id) => {
+  let els = [
+    document.querySelectorAll('[role="navigation"] a'),
+    document.querySelectorAll('[role="region"]')
+    ]
+  els.forEach(nl => {
+    nl.forEach(el => {
+      el.classList.remove('active')
+    })
+  })
+  let c = [
+    document.getElementById(id),
+    document.querySelector(`[aria-label="${id}"]`)
+  ]
+  c.forEach((el, i) => {
+    let h = document.getElementsByTagName('h1')[0]
+    console.log(el)
+    if(el === null) return
+    if(id === "contact") {
+      console.log('contact clicked!')
+      h.classList.remove('active')
+    } else {
+      h.classList.add('active')
+    }
+    el.classList.toggle('active')
+  })
+  let t = id === "contact" ? "photography" : id
+  document.getElementsByClassName('title')[0].innerHTML = t
+  document.title = t.charAt(0).toUpperCase() + t.slice(1) + " by Danielle Wahl"
+}
+
+let dl = document.location.href.split('#')[1]
+if(dl !== undefined) { toggleActive(dl) }
