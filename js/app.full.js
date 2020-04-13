f54074c7bbd3d2f13ffd8d611cb5147b0e1621da
Danny Wahl <danny@iywahl.com>
2020-04-13 15:58:38 -0600

    minify by default

---
+++ js/app.full.js
@@ -0,0 +1,30 @@
+const toggleActive = id => {
+  let els = [
+    document.querySelectorAll('nav a'),
+    document.querySelectorAll('.group')
+    ]
+  els.forEach(nl => {
+    nl.forEach(el => {
+      el.classList.remove('active')
+    })
+  })
+  let c = [
+    document.querySelector(`[href="#${id}"]`),
+    document.querySelector(`[id="${id}"]`)
+  ]
+  let h = document.getElementsByTagName('h1')[0]
+  c.forEach(el => {
+    if(el === null) return
+    if(id === "contact") {
+      h.classList.remove('active')
+    } else {
+      h.classList.add('active')
+    }
+    el.classList.toggle('active')
+  })
+  let t = id === "contact" ? "photography" : id
+  document.getElementsByClassName('title')[0].innerHTML = t
+  document.title = `${t.charAt(0).toUpperCase() + t.slice(1)} by Danielle Wahl`
+}
+let u = document.location.href.split('#')[1]
+if(u !== undefined) toggleActive(u)
